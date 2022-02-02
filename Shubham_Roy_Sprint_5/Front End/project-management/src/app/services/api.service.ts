560a2bf951d411978de66ae209385cbfaff5a71b
Shubham Roy <shubhamroy896@gmail.com>
2022-02-02 11:16:31 +0530

    Adding services

---
+++ Shubham_Roy_Sprint_5/Front End/project-management/src/app/services/api.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() { }
+
+  get(){
+
+  }
+
+  put(){
+
+  }
+
+
+  post(){
+
+  }
+
+  delete(){
+    
+  }
+}
